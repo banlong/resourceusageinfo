f720198bce5ba61458af26e628b03be26c649877
NGHIEP <nghiepnds@yahoo.com>
2016-06-10 15:17:42 -0700

    Add Memory Usage Info

--- memory.go
+++ memory.go
@@ -1,23 +1,30 @@
 package main
 
+//https://godoc.org/github.com/shirou/gopsutil has many ways modules to provide system information
+
 import (
 	"fmt"
 
 	"github.com/shirou/gopsutil/mem"
+	"github.com/shirou/gopsutil/cpu"
 	"resourceusageinfo/render"
 )
 
 func main() {
 	v, _ := mem.VirtualMemory()
-	s, _ := mem.SwapMemory()
-	// almost every return value is a struct
 	fmt.Println("VIRTUAL MEMORY")
 	fmt.Println("Total Memory: ", render.RenderInt64("#,###.",v.Total), "bytes")
 	fmt.Println("Used        : ", render.RenderInt64("#,###.",v.Used), "bytes")
 	fmt.Println("Used Percentage: : ", v.UsedPercent, "%")
 
-	fmt.Println("SWAP MEMORY")
-	fmt.Println("Total Memory: ", render.RenderInt64("#,###.",s.Total), "bytes")
-	fmt.Println("Used        : ", render.RenderInt64("#,###.",s.Used), "bytes")
-	fmt.Println("Used Percentage: : ", s.UsedPercent, "%")
+	fmt.Println("CPU")
+	cpus, _ := cpu.Counts(false)
+	percet, _ := cpu.Percent(0, true)
+	fmt.Println("CPUs: ", cpus)
+	fmt.Println("Usage: ", percet, "%")
+
+	timeStat, _ := cpu.Times(false)
+	fmt.Println("CPU 0: ", timeStat[0])
+
+
 }
